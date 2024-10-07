b90e38a0bf4be357038fa800568d7b6075e83faf
sima17 <simacodes2.0@gmail.com>
2024-10-07 22:43:22 +0530

    first commit

---
+++ src/bank/management/system/Signup.java
@@ -0,0 +1,75 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+public class Signup extends JFrame {
+    JTextField textName, textFname;
+      JDateChooser dateChooser;
+    Random ran=new Random();
+    long first4 =(ran.nextLong() % 9000L) +1000L;
+    String first = " " + Math.abs(first4);
+    Signup(){
+        super ("APPLICATION FORM");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(25,10,100,100);
+        add(image);
+
+        JLabel label1 = new JLabel("APPLICATION FORM NO."+ first);
+        label1.setBounds(160,20,600,40);
+        label1.setFont(new Font("Raleway",Font.BOLD,38));
+        add(label1);
+
+        JLabel label2 = new JLabel("Page 1");
+        label2.setFont(new Font("Ralway",Font.BOLD, 22));
+        label2.setBounds(330,70,600,30);
+        add(label2);
+
+        JLabel label3 = new JLabel("Personal Details");
+        label3.setFont(new Font("Raleway", Font.BOLD,22));
+        label3.setBounds(290,90,600,30);
+        add(label3);
+
+        JLabel labelName = new JLabel("Name :");
+        labelName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelName.setBounds(100,190,100,30);
+        add(labelName);
+
+        textName = new JTextField();
+        textName.setFont(new Font("Raleway",Font.BOLD, 14));
+        textName.setBounds(300,190,400,30);
+        add(textName);
+
+        JLabel labelfName = new JLabel("Father's Name :");
+        labelfName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelfName.setBounds(100,240,200,30);
+        add(labelfName);
+
+        textFname = new JTextField();
+        textFname.setFont(new Font("Raleway",Font.BOLD, 14));
+        textFname.setBounds(300,240,400,30);
+        add(textFname);
+
+        JLabel DOB = new JLabel("Date of Birth");
+        DOB.setFont(new Font("Raleway", Font.BOLD, 20));
+        DOB.setBounds(100,340,200,30);
+        add(DOB);
+
+
+
+        getContentPane().setBackground(new Color(222,255,228));
+        setLayout(null);
+        setSize(850,800);
+        setLocation(360,40);
+        setVisible(true);
+
+
+    }
+    public static void main(String[] args) {
+    new Signup();
+    }
+}
